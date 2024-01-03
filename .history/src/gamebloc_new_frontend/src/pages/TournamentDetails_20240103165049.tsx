fad965ab413d4cddc083376888a8ea273c091e76
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2024-01-03 16:51:44 +0100

    [Tournament Details UI design 42%]

--- .history/src/gamebloc_new_frontend/src/pages/TournamentDetails_20240103165049.tsx
+++ .history/src/gamebloc_new_frontend/src/pages/TournamentDetails_20240103165049.tsx
@@ -65,7 +65,7 @@ const TournamentDetail = () => {
                         alt=""
                         className="rounded-[0.625rem]"
                       />
-                      <div className="absolute flex top-4 left-4 bg-gradient-to-r justify-between items-center from-[#77536F] to-[#574151] rounded-md sm:rounded-xl py-1 px-2 sm:p-3 ">
+                      <div className="absolute flex top-4 left-4 bg-gradient-to-r justify-between items-center from-[#77536F] to-[#574151] rounded-xl py-1 px-2 sm:p-3 ">
                         <img src={`mdi_crowd.png`} className="m-0" alt="" />
                         <p className=" ml-[.5rem]  text-[0.6rem]  sm:text-sm text-white">
                           Crowd Funded
