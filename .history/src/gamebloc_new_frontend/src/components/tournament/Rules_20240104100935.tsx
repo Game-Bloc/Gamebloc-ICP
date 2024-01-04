4b09404fc66cad401aa204006179bbb37ee6b388
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2024-01-04 10:14:19 +0100

    [Added rule component to join tournament]

---
+++ .history/src/gamebloc_new_frontend/src/components/tournament/Rules_20240104100935.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+const Rules = () => {
+  return (
+    <div className="">
+      <p className="text-white">
+        👥 Team Composition: Teams consist of at least 6 players: 1 Team Lead
+        and 5 squad members. Each squad must have 3 primary players and 2
+        substitutes, allowing for a maximum of 12 players with up to 4 subs. 🚫
+        Rules: Weapons: No shotguns allowed. Respawn: Unlimited respawns to keep
+        the action intense. 🚷 Banned Classes: Ninja: Stealth capabilities
+        disrupt fair play. Defender: Overpowered defensive advantages. Trap
+        Master: Limits strategic mobility. Poltergeist: Unfair advantages in
+        avoiding engagements. 🚗 Banned Vehicles: Helicopter: Excessive mobility
+        disrupts fair competition. Tank: Overpowered firepower imbalance. ATV
+        (All-Terrain Vehicle): Rapid mobility gives an unfair edge. 🔐
+        Additional Regulations: Participants must adhere to fair play and
+        sportsmanship. The tournament will follow the latest CODM updates and
+        patches.
+      </p>
+    </div>
+  )
+}
+
+export default Rules
