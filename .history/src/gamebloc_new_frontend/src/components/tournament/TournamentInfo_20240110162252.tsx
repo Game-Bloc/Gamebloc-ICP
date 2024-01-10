2b0442607d8808f93b41cb02427e12abfb07a780
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2024-01-10 16:32:54 +0100

    [Tournament Info 30%]

--- .history/src/gamebloc_new_frontend/src/components/tournament/TournamentInfo_20240110162252.tsx
+++ .history/src/gamebloc_new_frontend/src/components/tournament/TournamentInfo_20240110162252.tsx
@@ -8,7 +8,7 @@ const TournamentInfo = () => {
           <div className=" w-full rounded-md bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] p-[.09rem]">
             <div className="flex flex-col w-full rounded-md  bg-primary-first pt-[.5rem] pl-[.5rem]">
               <p className="text-[.8rem]  text-white">Entry Fee</p>
-              <h1 className=" text-[2rem] sm:text-[3rem] font-valorant bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] text-[transparent] bg-clip-text  ">
+              <h1 className="text-[3rem] font-valorant bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] text-[transparent] bg-clip-text  ">
                 $10
               </h1>
             </div>
@@ -16,13 +16,13 @@ const TournamentInfo = () => {
           <div className=" w-full rounded-md bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] p-[.09rem]">
             <div className="flex flex-col w-full rounded-md  bg-primary-first pt-[.5rem] pl-[.5rem]">
               <p className="text-[.8rem]  text-white">Prize Pool</p>
-              <h1 className="text-[2rem] sm:text-[3rem] font-valorant bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] text-[transparent] bg-clip-text  ">
+              <h1 className="text-[3rem] font-valorant bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] text-[transparent] bg-clip-text  ">
                 $400
               </h1>
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-2 xl:grid-cols-3 gap-4 xl:gap-8 mt-4">
+        <div className="flex  gap-4 mt-4">
           <div className=" w-full rounded-md bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] p-[.09rem]">
             <div className="flex flex-col justify-center items-center w-full rounded-md  bg-primary-first pt-[.5rem] pl-[.5rem]">
               <p className="text-[.8rem]  text-white">1st</p>
--- .history/src/gamebloc_new_frontend/src/components/tournament/TournamentInfo_20240110162545.tsx
+++ .history/src/gamebloc_new_frontend/src/components/tournament/TournamentInfo_20240110162545.tsx
@@ -8,7 +8,7 @@ const TournamentInfo = () => {
           <div className=" w-full rounded-md bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] p-[.09rem]">
             <div className="flex flex-col w-full rounded-md  bg-primary-first pt-[.5rem] pl-[.5rem]">
               <p className="text-[.8rem]  text-white">Entry Fee</p>
-              <h1 className=" text-[2rem] sm:text-[3rem] font-valorant bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] text-[transparent] bg-clip-text  ">
+              <h1 className="text-[3rem] font-valorant bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] text-[transparent] bg-clip-text  ">
                 $10
               </h1>
             </div>
@@ -16,7 +16,7 @@ const TournamentInfo = () => {
           <div className=" w-full rounded-md bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] p-[.09rem]">
             <div className="flex flex-col w-full rounded-md  bg-primary-first pt-[.5rem] pl-[.5rem]">
               <p className="text-[.8rem]  text-white">Prize Pool</p>
-              <h1 className="text-[2rem] sm:text-[3rem] font-valorant bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] text-[transparent] bg-clip-text  ">
+              <h1 className="text-[3rem] font-valorant bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] text-[transparent] bg-clip-text  ">
                 $400
               </h1>
             </div>
