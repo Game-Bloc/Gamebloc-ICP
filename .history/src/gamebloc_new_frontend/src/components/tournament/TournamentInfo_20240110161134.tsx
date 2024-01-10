a1b2633892e10ad36530214411cb76887c860286
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2024-01-10 16:15:05 +0100

    [Tournament Info 20%]

--- .history/src/gamebloc_new_frontend/src/components/tournament/TournamentInfo_20240110161134.tsx
+++ .history/src/gamebloc_new_frontend/src/components/tournament/TournamentInfo_20240110161134.tsx
@@ -6,17 +6,17 @@ const TournamentInfo = () => {
       <div className="flex flex-col">
         <div className="flex gap-4">
           <div className=" w-full rounded-md bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] p-[.09rem]">
-            <div className="flex flex-col w-full rounded-md  bg-primary-first p-[..5rem] ">
+            <div className="flex flex-col w-full rounded-md  bg-primary-first p-4 ">
               <p className="text-[.8rem]  text-white">Entry Fee</p>
-              <h1 className="text-[3rem] font-valorant bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] text-[transparent] bg-clip-text  ">
+              <h1 className="text-[3rem] font-valorant bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] text-[transparent] bg-clip-text  mt-4">
                 $10
               </h1>
             </div>
           </div>
           <div className=" w-full rounded-md bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] p-[.09rem]">
-            <div className="flex flex-col w-full rounded-md  bg-primary-first p-[..5rem] ">
+            <div className="flex flex-col w-full rounded-md  bg-primary-first p-4 ">
               <p className="text-[.8rem]  text-white">Entry Fee</p>
-              <h1 className="text-[3rem] font-valorant bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] text-[transparent] bg-clip-text  ">
+              <h1 className="text-[3rem] font-valorant bg-gradient-to-b from-[#A380C4]  to-[#96C2FB] text-[transparent] bg-clip-text  mt-4">
                 $10
               </h1>
             </div>
