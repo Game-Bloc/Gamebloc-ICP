b963c6be363c333690b781e566a439c91a02de20
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2024-01-14 14:29:24 +0100

    [Fixed Blochooks function error]

--- .history/src/gamebloc_new_frontend/src/Functions/blochooks_20240101114257.tsx
+++ .history/src/gamebloc_new_frontend/src/Functions/blochooks_20240101114257.tsx
@@ -1,121 +1,113 @@
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { useAppDispatch } from "../redux/hooks"
-import { useAuth } from "../Auth/use-auth-client"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../redux/hooks";
+import { useAuth } from "../Auth/use-auth-client";
 import {
   addToActiveTournament,
   clearTournaments,
   updateActiveTournament,
-} from "../redux/slice/tournamentDataSlice"
+} from "../redux/slice/tournamentDataSlice";
 
 export const useFetchAllTournaments = () => {
-  const navigate = useNavigate()
-  const dispatch = useAppDispatch()
-  const { whoamiActor, isAuthenticated } = useAuth()
-  const [loading, setIsLoading] = useState(false)
-  const [nodata, setNoData] = useState(false)
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const { whoamiActor, isAuthenticated } = useAuth();
+  const [loading, setIsLoading] = useState(false);
+  const [nodata, setNoData] = useState(false);
 
   const fetchAllTournaments = async () => {
     if (isAuthenticated) {
       try {
-        setIsLoading(true)
-        console.log("get tournament was called")
-        dispatch(clearTournaments())
-        const tour: any = await whoamiActor.get_all_tournament()
-        console.log("Tour:", tour)
+        setIsLoading(true);
+        console.log("get tournament was called");
+        dispatch(clearTournaments());
+        const tour: any = await whoamiActor.get_all_tournament();
+        console.log("Tour:", tour);
         if (tour && tour.length !== 0) {
-          console.log("went")
+          console.log("went");
           for (const data of tour) {
             const tournamentData = {
               creator: data.creator,
-              end_date: data.end_date,
               entry_prize: data.entry_prize,
               game: data.game,
-              game_type: data.game_type,
               id_hash: data.id_hash,
               idx: data.idx,
               no_of_participants: Number(data.no_of_participants),
               no_of_winners: data.no_of_winners,
-              squad: data.squad.map((squad) => squad),
               starting_date: data.starting_date,
               status: data.status,
-              title: data.title,
               total_prize: Number(data.total_prize),
               tournament_rules: data.tournament_rules,
               tournament_type: data.tournament_type,
               users: data.user.map((user) => user),
               winners: data.winers.map((winner) => winner),
-            }
-            console.log(tournamentData)
-            dispatch(addToActiveTournament(tournamentData))
+            };
+            console.log(tournamentData);
+            dispatch(addToActiveTournament(tournamentData));
           }
-          setIsLoading(false)
+          setIsLoading(false);
         } else {
-          setNoData(true)
+          setNoData(true);
         }
       } catch (err) {
-        console.log("Error:", err)
+        console.log("Error:", err);
       } finally {
-        setIsLoading(false)
+        setIsLoading(false);
       }
     } else {
-      console.log("err: User not authenticated")
+      console.log("err: User not authenticated");
     }
-  }
+  };
 
-  return { fetchAllTournaments, loading, nodata }
-}
+  return { fetchAllTournaments, loading, nodata };
+};
 
 export const useUpdateTournament = () => {
-  const { whoamiActor, isAuthenticated } = useAuth()
-  const dispatch = useAppDispatch()
-  const [updating, setUpdating] = useState<boolean>(false)
-  const [noData, setNoData] = useState<boolean>(false)
+  const { whoamiActor, isAuthenticated } = useAuth();
+  const dispatch = useAppDispatch();
+  const [updating, setUpdating] = useState<boolean>(false);
+  const [noData, setNoData] = useState<boolean>(false);
 
   const updateTournament = async () => {
     if (isAuthenticated) {
       try {
-        setUpdating(true)
-        const update: any = await whoamiActor.get_all_tournament()
+        setUpdating(true);
+        const update: any = await whoamiActor.get_all_tournament();
         if (update && update.length !== 0) {
-          console.log("update function working")
+          console.log("update function working");
           for (const data of update) {
             const tournamentData = {
               creator: data.creator,
-              end_date: data.end_date,
               entry_prize: data.entry_prize,
               game: data.game,
-              game_type: data.game_type,
               id_hash: data.id_hash,
               idx: data.idx,
               no_of_participants: Number(data.no_of_participants),
               no_of_winners: data.no_of_winners,
-              squad: data.squad.map((squad) => squad),
               starting_date: data.starting_date,
               status: data.status,
-              title: data.title,
               total_prize: Number(data.total_prize),
               tournament_rules: data.tournament_rules,
               tournament_type: data.tournament_type,
               users: data.user.map((user) => user),
               winners: data.winers.map((winner) => winner),
-            }
-            console.log(tournamentData)
-            dispatch(updateActiveTournament(tournamentData))
+            };
+            console.log(tournamentData);
+            dispatch(updateActiveTournament(tournamentData));
           }
-          setUpdating(false)
+          setUpdating(false);
         } else {
-          setNoData(true)
+          setNoData(true);
         }
       } catch (err) {
-        console.log("Error:", err)
+        console.log("Error:", err);
       } finally {
-        setUpdating(false)
+        setUpdating(false);
       }
     } else {
-      console.log("User not authenticated")
+      console.log("User not authenticated");
     }
-  }
+  };
 
-  return { updateTournament, updating, setNoData }
-}
+  return { updateTournament, updating, setNoData };
+};
