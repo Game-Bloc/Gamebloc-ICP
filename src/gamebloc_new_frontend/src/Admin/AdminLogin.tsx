059a23f2441c920f932d287c2d4b91f3fe6a897a
Deonorla <deonoluleye@gmail.com>
2024-04-05 09:29:46 +0100

    [new admin design- 80%]

--- src/gamebloc_new_frontend/src/Admin/AdminLogin.tsx
+++ src/gamebloc_new_frontend/src/Admin/AdminLogin.tsx
@@ -59,8 +59,53 @@ const AdminLogin = () => {
   }
 
   return (
-    <div>
-      <div
+    <div className="w-full h-fit">
+      <div className="flex ">
+        <div className="relative w-[50%]">
+          <img src={`style.png`} alt="" className="h-screen w-full" />
+          <div className="absolute flex flex-col justify-center items-center top-0 bottom-0 right-0 left-0 m-auto">
+            <img
+              src={`gamelogo.png`}
+              alt=""
+              className="w-[15rem] h-[15rem] m-0"
+            />
+            <h1 className="font-valorant mt-[2rem] text-primary-second text-[2.2rem] text-semibold">
+              YOU CONTROL
+            </h1>
+          </div>
+        </div>
+        <div className="w-[50%] flex flex-col justify-center items-center">
+          <img src={`logo.png`} alt="" className="m-0 w-[15rem]" />
+          <div className="flex flex-col w-[80%] mt-4">
+            <div className="flex items-center pl-[.5rem] h-[2rem] border-[#F6B8FC]/30 bg-[#f6b8fc7a]/20 border border-solid rounded-[8px] w-full">
+              <input
+                className="border-none bg-[transparent] text-white/80 placeholder:text-[0.8rem] placeholder:text-white/80 focus:outline-none focus:ring-0 text-[0.8rem] appearance-none w-full"
+                type="text"
+                placeholder="Username"
+                onChange={onChangeUsername}
+                value={userName}
+              />
+            </div>
+            <div className="flex items-center mt-8 pl-[.5rem] h-[2rem] border-[#F6B8FC]/30 bg-[#f6b8fc7a]/20 border border-solid rounded-[8px] w-full">
+              <input
+                className="border-none bg-[transparent] text-white/80 placeholder:text-[0.8rem] placeholder:text-white/80 focus:outline-none focus:ring-0 text-[0.8rem] appearance-none w-full"
+                type="text"
+                placeholder="Password"
+                value={password}
+                onChange={onChangePassword}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AdminLogin
+
+{
+  /* <div
         className="relative z-10"
         aria-labelledby="modal-title"
         role="dialog"
@@ -75,12 +120,16 @@ const AdminLogin = () => {
                     src={`welcome.png`}
                     alt=""
                     className="rounded-t-[25px]"
-                  />
-                  {/* <RiCloseFill
+                  /> */
+}
+{
+  /* <RiCloseFill
                     //   onClick={modal}
                     className="absolute text-white right-4 text-[1rem] top-4 cursor-pointer"
-                  /> */}
-                  <div className="">
+                  /> */
+}
+{
+  /* <div className="">
                     <img
                       src={`gamelogo.png`}
                       className="-mt-4 mb-[.3rem] w-[3rem] h-[3rem]"
@@ -141,9 +190,5 @@ const AdminLogin = () => {
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  )
+      </div> */
 }
-
-export default AdminLogin
