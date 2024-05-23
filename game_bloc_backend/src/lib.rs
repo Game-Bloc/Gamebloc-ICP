fb55c4145e1ea12e9cf2759513e1ecb5a5020e35
marvincd <mharvehll@gmail.com>
2024-05-23 15:15:47 +0100

    added functions for restructuring tournaments back into the code base

--- game_bloc_backend/src/lib.rs
+++ game_bloc_backend/src/lib.rs
@@ -802,26 +802,26 @@ fn assign_solo_points_and_end_lobby(tournament_id: String, mut user_id_and_point
     }
 }
 
-// #[update]
-// fn structure_tournament_into_duo_lobbies(name: String, id: String) {
-//     TOURNAMENT_STORE.with(|tournament_store| {
-//         let mut tournament = tournament_store.borrow().get(&id).cloned().unwrap();
-//         tournament.user.push(name);
-//         tournament_store.borrow_mut().insert(id, tournament);
-//     });
-// }
-//
-// #[update]
-// fn structure_tournament_into_squad_lobbies(squad_id: String, id: String) {
-//     TOURNAMENT_STORE.with(|tournament_store| {
-//         let mut tournament = tournament_store.borrow().get(&id).cloned().unwrap();
-//         SQUAD_STORE.with(|squad_store| {
-//             let squad = squad_store.borrow().get(&squad_id).cloned().unwrap();
-//             tournament.squad.push(squad);
-//         });
-//         tournament_store.borrow_mut().insert(id, tournament);
-//     });
-// }
+#[update]
+fn structure_tournament_into_duo_lobbies(name: String, id: String) {
+    TOURNAMENT_STORE.with(|tournament_store| {
+        let mut tournament = tournament_store.borrow().get(&id).cloned().unwrap();
+        tournament.user.push(name);
+        tournament_store.borrow_mut().insert(id, tournament);
+    });
+}
+
+#[update]
+fn structure_tournament_into_squad_lobbies(squad_id: String, id: String) {
+    TOURNAMENT_STORE.with(|tournament_store| {
+        let mut tournament = tournament_store.borrow().get(&id).cloned().unwrap();
+        SQUAD_STORE.with(|squad_store| {
+            let squad = squad_store.borrow().get(&squad_id).cloned().unwrap();
+            tournament.squad.push(squad);
+        });
+        tournament_store.borrow_mut().insert(id, tournament);
+    });
+}
 
 
 //setting mods and managing admins
