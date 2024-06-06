c48ea7fb4d5d3ea889a4bdc620f0d3f4c541732a
Nailer <ajeemmanuel221@gmail.com>
2024-06-06 19:06:32 +0100

    intern branch created

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ dfx identity use minter
 MINT_ACC=$(dfx ledger account-id)
 export MINT_ACC
 
-dfx identity use finisher
+dfx identity use default
 
 LEDGER_ACC=$(dfx ledger account-id)
 export LEDGER_ACC
@@ -25,17 +25,17 @@ candid-extractor target/wasm32-unknown-unknown/release/game_bloc_backend.wasm > 
 dfx deploy icp_ledger --argument "(variant {Init =record {minting_account = \"${MINT_ACC}\";
 initial_values = vec { record {  \"${LEDGER_ACC}\";
 record { e8s=100_000_000_000 } } } ; archive_options = opt record {num_blocks_to_archive = 1000000; trigger_threshold = 1000000; \
-  controller_id = principal  \"${ARCHIVE_CONTROLLER}\"; }; send_whitelist = vec {}}})" --specified-id ryjl3-tyaaa-aaaaa-aaaba-cai
+controller_id = principal  \"${ARCHIVE_CONTROLLER}\"; }; send_whitelist = vec {}}})" --specified-id ryjl3-tyaaa-aaaaa-aaaba-cai
 
 dfx deploy icp_index --specified-id qhbym-qaaaa-aaaaa-aaafq-cai --argument '(record {ledger_id = principal "ryjl3-tyaaa-aaaaa-aaaba-cai"})'
 
 dfx canister --network local call icp_ledger icr1_transfer '
-    (record {
-      to=(record {
-        owner=(principal "6cxww-biaaa-aaaal-adebq-cai")
-        });
-        amount=500_000
-    })'
+  (record {
+    to=(record {
+      owner=(principal "6cxww-biaaa-aaaal-adebq-cai")
+      });
+      amount=500_000
+})'
 
 dfx deploy
 
