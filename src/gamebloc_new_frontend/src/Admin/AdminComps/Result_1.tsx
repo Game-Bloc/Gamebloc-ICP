65f6af82a20e4c5935b8a31781141af1598a868f
Deonorla <deonoluleye@gmail.com>
2024-07-02 12:29:21 +0100

    [Tournament result for squad 90%]

---
+++ src/gamebloc_new_frontend/src/Admin/AdminComps/Result_1.tsx
@@ -0,0 +1,82 @@
+import React from "react"
+import { useGameblocHooks } from "../../Functions/gameblocHooks"
+import FallbackLoading from "../../components/Modals/FallBackLoader"
+import { ConfigProvider, Table, theme } from "antd"
+type prop = {
+  tourData: any
+}
+const Result_1 = ({ tourData }: prop) => {
+  const { isLoading, multiSelect_user_profile } = useGameblocHooks()
+
+  // Flatten and map the dataSource to the result array
+  const result = tourData.flatMap((state) =>
+    state.squad_points.flatMap((innerArray) =>
+      innerArray.map(([squad_name, id, pointsObject]) => ({
+        id,
+        name: squad_name,
+        squad_id: id.substring(0, 3) + "......" + id.substring(23, 26),
+        killPoints: pointsObject.kill_points,
+        totalPoints: pointsObject.total_points,
+        positionPoints: pointsObject.position_points,
+      })),
+    ),
+  )
+  // Sort the result array by killPoints in descending order
+  const sortedResult = result.sort((a, b) => b.totalPoints - a.totalPoints)
+
+  // Add a position field based on the sorted array index
+  const resultWithPosition = sortedResult.map((item, index) => ({
+    ...item,
+    position: index + 1,
+  }))
+
+  const columns = [
+    {
+      title: "Position",
+      dataIndex: "position",
+      key: "position",
+    },
+    { title: "Squad Name", dataIndex: "name", key: "name" },
+    { title: "Squad Id", dataIndex: "squad_id", key: "squad_id" },
+    {
+      title: "Position Points",
+      dataIndex: "positionPoints",
+      key: "positionPoints",
+    },
+    { title: "Kill Points", dataIndex: "killPoints", key: "killPoints" },
+    { title: "Total Points", dataIndex: "totalPoints", key: "totalPoints" },
+  ]
+  if (isLoading) {
+    return (
+      <div className="w-full h-screen flex justify-center items-center">
+        <FallbackLoading />
+      </div>
+    )
+  } else {
+    return (
+      <div className="mt-4">
+        <ConfigProvider
+          theme={{
+            algorithm: theme.darkAlgorithm,
+            token: {
+              colorPrimaryActive: "#F6B8FC",
+              colorPrimary: "#F6B8FC",
+              colorPrimaryHover: "#F6B8FC",
+              colorText: "#fff",
+            },
+          }}
+        >
+          <Table
+            // rowSelection={rowSelection}
+            columns={columns}
+            rowClassName={() => "rowClassName1"}
+            dataSource={resultWithPosition}
+            rowKey={"position"}
+          />
+        </ConfigProvider>
+      </div>
+    )
+  }
+}
+
+export default Result_1
--- src/gamebloc_new_frontend/src/Admin/AdminComps/SquadResult.tsx
+++ src/gamebloc_new_frontend/src/Admin/AdminComps/SquadResult.tsx
@@ -1,22 +1,25 @@
 import { ConfigProvider, Tabs, TabsProps } from "antd"
 import React from "react"
+import Result_1 from "./Result_1"
+import Result_2 from "./Result_2"
 
 type prop = {
   tourData: any
 }
-const items: TabsProps["items"] = [
-  {
-    key: "1",
-    label: `Squads Result`,
-    children: "squads",
-  },
-  {
-    key: "2",
-    label: `Players Result`,
-    children: "players",
-  },
-]
+
 const SquadResult = ({ tourData }: prop) => {
+  const items: TabsProps["items"] = [
+    {
+      key: "1",
+      label: `Squads Result`,
+      children: <Result_1 tourData={tourData} />,
+    },
+    {
+      key: "2",
+      label: `Players Result`,
+      children: <Result_2 tourData={tourData} />,
+    },
+  ]
   return (
     <div className="mt-4">
       <ConfigProvider
