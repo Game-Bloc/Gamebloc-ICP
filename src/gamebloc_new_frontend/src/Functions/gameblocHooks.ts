8777bd54adb04c3d74a5a20569b91365802d7602
Deonorla <deonoluleye@gmail.com>
2024-07-14 13:31:47 +0100

    [site optimization]

--- src/gamebloc_new_frontend/src/Functions/gameblocHooks.ts
+++ src/gamebloc_new_frontend/src/Functions/gameblocHooks.ts
@@ -827,7 +827,7 @@ export const useGameblocHooks = () => {
 
   const end_tournament = async (id: string, principal_id: Principal) => {
     try {
-      await whoamiActor2.end_tournament("", principal)
+      // await whoamiActor2.end_tournament("", principal)
     } catch (error) {}
   }
 
