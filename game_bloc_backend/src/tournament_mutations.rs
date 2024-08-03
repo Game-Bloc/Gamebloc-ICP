e00b648066203b92fb4b562d3d7df6cd2857bc63
marvincd <mharvehll@gmail.com>
2024-08-03 12:33:52 +0100

    missing function hot fix

--- game_bloc_backend/src/tournament_mutations.rs
+++ game_bloc_backend/src/tournament_mutations.rs
@@ -220,7 +220,7 @@ pub fn end_tournament(id: String, principal: Principal, number_of_winners:u8)
 
 
 #[update]
-pub fn archive_tournament(id: String,)
+pub fn archive_tournament(id: String)
 {
     TOURNAMENT_STORE.with(|tournament_store| {
         let mut tournament = tournament_store.borrow().get(&id).cloned().unwrap();
