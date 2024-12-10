120e1dc4570ac96a3ffb8a3a22154ba2cca41144
Maroof Kadiri <kadirimaroof@gmail.com>
2024-12-10 20:59:44 +0100

    added Landing Page Animations

--- src/gamebloc_new_frontend/src/components/onePager/Hero.tsx
+++ src/gamebloc_new_frontend/src/components/onePager/Hero.tsx
@@ -1,10 +1,23 @@
 import React from "react"
+import { motion } from "motion/react"
 
 interface Props {
   setModal: any
 }
 
 const Hero = ({ setModal }: Props) => {
+  const dropVariants = {
+    initial: {scale: 1.5 },
+    animate: {
+      scale: 1,
+      transition: {
+        type: "spring",
+        stiffness: 100,
+        damping: 10,
+      },
+    },
+  }
+
   return (
     <div className="relative bg-transparent ">
       <div className="flex flex-col justify-center items-center mt-40 lg:mt-[15rem]">
@@ -18,7 +31,13 @@ const Hero = ({ setModal }: Props) => {
           alt=""
           className="absolute left-0 right-0 top-[-9rem] lg:top-[-11rem] w-full  lg:w-[35%] opacity-75"
         />
-        <div className="flex flex-col justify-center items-center mx-4 lg:mx-0">
+        <motion.div
+          className="flex flex-col justify-center items-center mx-4 lg:mx-0"
+          variants={dropVariants}
+          initial="initial"
+          whileInView="animate"
+          viewport={{ once: true }}
+        >
           <p className="text-white text-[0.7rem] lg:text-lg mb-[0.5rem] md:mb-[1rem] bg-transparent">
             Next-Generation Gaming HUB. Compete, Win, Earn.
           </p>
@@ -35,7 +54,7 @@ const Hero = ({ setModal }: Props) => {
             <p className="text-[0.65rem] sm:text-[.85rem]">Get Started</p>
             <img src={`details.png`} alt="" className="m-0 w-[.75rem] sm:w-6" />
           </button>
-        </div>
+        </motion.div>
       </div>
       <div className="relative flex flex-col justify-center items-center">
         <img
@@ -50,13 +69,33 @@ const Hero = ({ setModal }: Props) => {
           className="absolute -z-20 left-0 right-0 top-[10rem] lg:top-[16rem] "
         />
         <div className="flex gap-8  justify-center lg:justify-between  items-center w-full lg:w-1/2 mt-4 lg:mt-[4rem]  mx-4 lg:mx-0">
-          <img src={`logo.png`} alt="" className="w-[5rem] md:w-32 m-0 " />
-          <img
+          <motion.img
+            initial={{ y: 60, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+            src={`logo.png`}
+            alt=""
+            className="w-[5rem] md:w-32 m-0 "
+          />
+          <motion.img
+            initial={{ y: 60, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1.5 }}
+            viewport={{ once: true }}
             src={`sabertooth.png`}
             alt=""
             className="w-[5rem]   md:w-[10rem] m-0 "
           />
-          <img src={`dfinity.png`} alt="" className="w-[5rem] md:w-32 m-0 " />
+          <motion.img
+            initial={{ y: 60, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 2 }}
+            viewport={{ once: true }}
+            src={`dfinity.png`}
+            alt=""
+            className="w-[5rem] md:w-32 m-0 "
+          />
         </div>
       </div>
     </div>
