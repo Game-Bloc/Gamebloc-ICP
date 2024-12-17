9ac94b367977e75cbcc98ee17b892fd77d8ad870
Maroof Kadiri <kadirimaroof@gmail.com>
2024-12-17 23:20:02 +0100

    Added vids to tutorial page

--- src/gamebloc_new_frontend/src/components/dashboardComps/Tutorials/Tutorials.tsx
+++ src/gamebloc_new_frontend/src/components/dashboardComps/Tutorials/Tutorials.tsx
@@ -2,10 +2,11 @@ import { Swiper, SwiperClass, SwiperSlide } from "swiper/react"
 import "swiper/css"
 import "swiper/swiper-bundle.css"
 import "swiper/css/navigation"
+import "./Tutorial.css"
 import { Navigation, Autoplay } from "swiper/modules"
 import React, { useCallback, useState } from "react"
-// import { ChevronRight, ChevronLeft } from "lucide-react"
-import {Link} from "react-router-dom"
+import { ChevronRight, ChevronLeft } from "lucide-react"
+import { Link } from "react-router-dom"
 
 const Tutorials = () => {
   const [swiperRef, setSwiperRef] = useState<SwiperClass>()
@@ -18,26 +19,34 @@ const Tutorials = () => {
   }, [swiperRef])
 
   return (
-    <div className="w-full flex flex-col gap-4 my-7 bg-[#040D17] rounded-[1.5rem]">
-      <div className="flex justify-between">
+    <div className="w-full flex flex-col gap-8 my-7 bg-[#040D17] rounded-[1.5rem] py-4 px-3">
+      <div className="flex justify-between items-center">
         <h2 className="text-white text-lg">Know your way around Gamebloc</h2>
-        {/* <div>
-          <div className="rounded-md border p-2">
-            <ChevronLeft />
+        <div className="flex gap-3">
+          <div
+            onClick={handlePrevious}
+            className="rounded-md border p-2 border-[#F6B8FC] hidden lg:block cursor-pointer"
+          >
+            <ChevronLeft
+              className="hover:text-black transition-colors"
+              color="#F6B8FC"
+            />
           </div>
-          <div className="rounded-md border p-2">
-            <ChevronRight color="#F6B8FC" />
+          <div
+            onClick={handleNext}
+            className="rounded-md border p-2 border-[#F6B8FC] hidden lg:block cursor-pointer"
+          >
+            <ChevronRight
+              className="hover:text-black transition-colors"
+              color="#F6B8FC"
+            />
           </div>
-        </div> */}
+        </div>
       </div>
       <div className="w-full flex justify-center">
         <Swiper
           onSwiper={setSwiperRef}
           slidesPerView={1}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: true,
-          }}
           modules={[Autoplay]}
           loop={true}
           breakpoints={{
@@ -46,12 +55,12 @@ const Tutorials = () => {
               spaceBetween: 20,
             },
           }}
-          className="mySwiper mx-auto"
+          className="mySwiperr mx-auto"
         >
-          <SwiperSlide className="w-[300px] lg:w-[480px] bg-">
+          <SwiperSlide className="w-[300px] lg:w-[480px] !bg-transparent">
             <div className="w-full flex flex-col gap-3 justify-center">
               <iframe
-              className="w-full h-[220px]"             
+                className="w-full h-[220px] rounded-lg"
                 src="https://www.youtube.com/embed/cdUxwm-WjHo?si=sqRJ3dvWALhH7lw3"
                 title="YouTube video player"
                 frameBorder="0"
@@ -64,26 +73,10 @@ const Tutorials = () => {
               </p>
             </div>
           </SwiperSlide>
-          <SwiperSlide className="w-[300px] lg:w-[480px]">
-            <div className="w-full flex flex-col gap-3 justify-center">
+          <SwiperSlide className="w-[300px] lg:w-[480px] !bg-transparent">
+            <div className="w-full flex flex-col gap-3 justify-center ">
               <iframe
-              className="w-full h-[220px]"
-                src="https://www.youtube.com/embed/JbUwfy5TjK8?si=LnS5Ib-JGz9V37p1"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin"
-                allowFullScreen
-              ></iframe>
-              <p className="text-base text-center text-white">
-                How to withdraw ICP from Gameblock
-              </p>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="w-[300px] lg:w-[480px]">
-            <div className="w-full flex flex-col gap-3 justify-center">
-              <iframe
-              className="w-full h-[220px]"
+                className="w-full h-[220px] rounded-lg"
                 src="https://www.youtube.com/embed/de61Hplfv-8?si=jw8yOoO3I0cWnMiK"
                 title="YouTube video player"
                 frameBorder="0"
@@ -92,14 +85,34 @@ const Tutorials = () => {
                 allowFullScreen
               ></iframe>
               <p className="text-base text-center text-white">
-                How to withdraw ICP from Gameblock
+                How to buy ICP on Gamebloc
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="w-[300px] lg:w-[480px] !bg-transparent">
+            <div className="w-full flex flex-col gap-3 justify-center">
+              <iframe
+                className="w-full h-[220px] rounded-lg"
+                src="https://www.youtube.com/embed/JbUwfy5TjK8?si=LnS5Ib-JGz9V37p1"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+              <p className="text-base text-center text-white">
+                How to Create a Squad on Gamebloc
               </p>
             </div>
           </SwiperSlide>
         </Swiper>
       </div>
-      <div className="w-full ">
-        <Link to="howto"><button className=" mx-auto text-[#F6B8FC] text-center text-base px-3 py-2 border border-[#F6B8FC] rounded-lg">See All</button></Link>
+      <div className="w-full flex justify-center">
+        <Link to="/howto">
+          <button className=" mx-auto hover:text-black hover:bg-[#F6B8FC] text-[#F6B8FC] text-center text-base px-3 py-2 border border-[#F6B8FC] rounded-lg">
+            See All
+          </button>
+        </Link>
       </div>
     </div>
   )
--- src/gamebloc_new_frontend/src/pages/HowTo.tsx
+++ src/gamebloc_new_frontend/src/pages/HowTo.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react"
+import { ArrowLeft } from "lucide-react"
+import { useNavigate } from "react-router-dom"
 
 const HowTo = () => {
+  const navigate = useNavigate()
   return (
-    <div className='w-scr'>
-
+    <div className="flex flex-col gap-8 sm:px-10 py-8 w-screen">
+      <div onClick={() => navigate(-1)} className="flex gap-4 items-center text-[#F6B8FC] cursor-pointer px-10 sm:px-0">
+        <ArrowLeft className="color-[#F6B8FC]" />
+        <span className="text-sm" >Go Back</span>
+      </div>
+      <div className="flex flex-col gap-5 px-10 sm:px-0">
+        <h1 className="text-white text-4xl font-semibold">Tutorial Videos</h1>
+        <p className="text-white text-base">
+          Here are videos to help you Know and find your way Around GameBloc
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row gap-8 flex-wrap mx-auto">
+        <div className="w-[300px] xl:w-[480px] flex flex-col gap-4 justify-center">
+          <iframe
+            className="w-full h-[220px] rounded-lg"
+            src="https://www.youtube.com/embed/cdUxwm-WjHo?si=sqRJ3dvWALhH7lw3"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+          <p className="text-base text-center text-white">
+            How to withdraw ICP from Gamebloc
+          </p>
+        </div>
+        <div className="w-[300px] xl:w-[480px] flex flex-col gap-4 justify-center">
+          <iframe
+            className="w-full h-[220px] rounded-lg"
+            src="https://www.youtube.com/embed/JbUwfy5TjK8?si=LnS5Ib-JGz9V37p1"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+          <p className="text-base text-center text-white">
+            How to Create a Squad on GameBloc
+          </p>
+        </div>
+        <div className="w-[300px] xl:w-[480px] flex flex-col gap-4 justify-center">
+          <iframe
+            className="w-full h-[220px] rounded-lg"
+            src="https://www.youtube.com/embed/de61Hplfv-8?si=jw8yOoO3I0cWnMiK"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+          <p className="text-base text-center text-white">
+            How to buy ICP on GameBloc
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
