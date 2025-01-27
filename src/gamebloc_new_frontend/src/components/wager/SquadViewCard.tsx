79f8e7a151925af3ffc6eb9184ab2b4be1586dea
Deonorla <deonoluleye@gmail.com>
2025-01-27 09:11:42 +0100

    [Gamer stats card 75%]

--- src/gamebloc_new_frontend/src/components/wager/SquadViewCard.tsx
+++ src/gamebloc_new_frontend/src/components/wager/SquadViewCard.tsx
@@ -12,17 +12,29 @@ const SquadViewCard = () => {
 
   return (
     <div className=" flex flex-col ">
-      <div
-        onClick={handleToggle}
-        className="bg-[#1E1E21] flex justify-between items-center mt-4 px-4 py-2 w-full rounded-full cursor-pointer"
-      >
-        <p className="text-[.85rem] text-[#A1A1AA] font-bold">Fire Squad</p>
+      <div className="bg-[#1E1E21] flex justify-between items-center mt-4 px-4 py-2 w-full rounded-xl cursor-pointer">
+        <div className="flex flex-col">
+          <p className="ml-1  mt-3 text-[.85rem] lg:text-[1rem] text-[#A1A1AA] font-bold">
+            Fire Squad
+          </p>
+          <div className="flex gap-4 mt-3">
+            <button
+              onClick={handleToggle}
+              className="py-2 px-2  bg-primary-second text-black w-[10rem]  text-xs sm:text-sm rounded-xl "
+            >
+              Squad info
+            </button>
+            <button className="py-2 px-4  bg-primary-second text-black w-[10rem]  text-xs sm:text-sm rounded-xl ">
+              Place Bet
+            </button>
+          </div>
+        </div>
         <motion.div
           initial={{ rotate: 0 }}
           animate={{ rotate: showCard ? 180 : 0 }}
           transition={{ duration: 0.3 }}
         >
-          <FaChevronDown className="text-[#A1A1AA]" />
+          <FaChevronDown onClick={handleToggle} className="text-[#A1A1AA]" />
         </motion.div>
       </div>
 
