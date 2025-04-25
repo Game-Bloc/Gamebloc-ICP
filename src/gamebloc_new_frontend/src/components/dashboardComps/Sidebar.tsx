080f7cc16eed0f9857f28fc2d50500732da62f56
marvincd <mharvehll@gmail.com>
2025-04-25 15:28:33 +0100

    removed buggy world chat feature

--- src/gamebloc_new_frontend/src/components/dashboardComps/Sidebar.tsx
+++ src/gamebloc_new_frontend/src/components/dashboardComps/Sidebar.tsx
@@ -26,11 +26,11 @@ const Sidebar = () => {
         { name: "CrowdFunded", link: "/active-tournament" },
       ],
     },
-    {
-      name: "World Chat",
-      link: isAuthenticated ? "/world-chat" : "",
-      icon: HiChatBubbleOvalLeft,
-    },
+    // {
+    //   name: "World Chat",
+    //   link: isAuthenticated ? "/world-chat" : "",
+    //   icon: HiChatBubbleOvalLeft,
+    // },
     {
       name: "Leaderboard",
       action: () => {},
